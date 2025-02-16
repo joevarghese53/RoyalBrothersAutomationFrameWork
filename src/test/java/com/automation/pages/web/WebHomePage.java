36a2d8830d811ad3aa27c410cf02ed6faa9a4958
joe varghese <joevarghese53@gmail.com>
2025-02-16 16:15:20 +0530

    Rental Location Feature 3

--- src/test/java/com/automation/pages/web/WebHomePage.java
+++ src/test/java/com/automation/pages/web/WebHomePage.java
@@ -3,6 +3,7 @@ package com.automation.pages.web;
 import com.automation.pages.common.BasePage;
 import com.automation.pages.ui.HomePage;
 import com.automation.utils.ConfigReader;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -12,13 +13,6 @@ public class WebHomePage extends BasePage implements HomePage {
     WebElement locationSearchField;
 
     String locationXpath = "//p[text()=' %s']";
-
-    @FindBy(xpath = "//a[contains(@class,\"current-city\")]")
-    WebElement locationBtn;
-
-    @FindBy(xpath = "//a[contains(@class,\"current-city\")]/span")
-    WebElement location;
-
 
     @Override
     public void openApplication() {
@@ -54,11 +48,11 @@ public class WebHomePage extends BasePage implements HomePage {
 
     @Override
     public void clickOnLocationButton() {
-        locationBtn.click();
+
     }
 
     @Override
     public boolean verifyUpdatedLocation() {
-        return location.getText().toUpperCase().equals(ConfigReader.getConfigValue("booking.updatedCity"));
+
     }
 }
