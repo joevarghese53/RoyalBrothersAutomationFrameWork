8fc11d66532f024d3bee97e6751d71ec60e11cf3
joe varghese <joevarghese53@gmail.com>
2025-02-24 11:03:06 +0530

    RB complete

--- src/test/java/com/automation/pages/android/AndroidSelectedProductPage.java
+++ src/test/java/com/automation/pages/android/AndroidSelectedProductPage.java
@@ -9,6 +9,12 @@ import org.openqa.selenium.support.FindBy;
 
 public class AndroidSelectedProductPage extends BasePage implements SelectedProductPage {
 
+    @FindBy(xpath = "//android.widget.ListView[@resource-id=\"product-grid\"]/android.view.View[1]/android.view.View")
+    WebElement firstProd;
+
+    @FindBy(xpath = "//android.widget.ListView[@resource-id=\"product-grid\"]/android.view.View[2]/android.view.View")
+    WebElement secondProd;
+
     @FindBy(xpath = "(//android.widget.TextView[@text=\"CHOOSE OPTIONS\"])[1]/../preceding-sibling::android.view.View/android.widget.TextView")
     WebElement firstProdName;
 
@@ -27,41 +33,43 @@ public class AndroidSelectedProductPage extends BasePage implements SelectedProd
     @FindBy(xpath = "(//android.widget.TextView[@text=\"CHOOSE OPTIONS\"])[2]/../preceding-sibling::android.widget.ListView//android.widget.TextView[2]")
     WebElement secondProdPrice;
 
-    String product="//android.widget.TextView[@text=\"%s\"]";
+    String product = "//android.widget.TextView[@text=\"%s\"]";
+
     @Override
     public boolean isSelectedProdPageDisplayed(String prod) {
-        return isDisplayed(product,prod);
+        System.out.println(String.format(product, prod));
+        return isDisplayed(product, prod);
     }
 
     @Override
     public void clickFirstProduct() {
         System.out.println(firstProdPrice.getText());
-        ConfigReader.setConfigValue("first.prod.name",firstProdName.getText());
+        ConfigReader.setConfigValue("first.prod.name", firstProdName.getText());
         String price;
-        if(firstProdPrice.getText().contains(",")){
-            price=firstProdPrice.getText().replace(",","");
+        if (firstProdPrice.getText().contains(",")) {
+            price = firstProdPrice.getText().replace(",", "");
+        } else {
+            price = firstProdPrice.getText();
         }
-        else{
-            price=firstProdPrice.getText();
-        }
-        ConfigReader.setConfigValue("first.prod.price", String.valueOf(Float.parseFloat(price.trim().replace("Rs.",""))));
+        ConfigReader.setConfigValue("first.prod.price", String.valueOf(Float.parseFloat(price.trim().replace("Rs.", ""))));
 //        WebElement chooseOpt=driver.findElement(By.xpath("(//quick-view-button)[1]"));
-        actions.moveToElement(firstProdName).click().build().perform();
+//        actions.moveToElement(firstProdName).build().perform();
+        firstProd.click();
     }
 
     @Override
     public void clickSecondProduct() {
         System.out.println(secondProdPrice.getText());
-        ConfigReader.setConfigValue("second.prod.name",secondProdName.getText());
+        ConfigReader.setConfigValue("second.prod.name", secondProdName.getText());
         String price;
-        if(secondProdPrice.getText().contains(",")){
-            price=secondProdPrice.getText().replace(",","");
+        if (secondProdPrice.getText().contains(",")) {
+            price = secondProdPrice.getText().replace(",", "");
+        } else {
+            price = secondProdPrice.getText();
         }
-        else{
-            price=secondProdPrice.getText();
-        }
-        ConfigReader.setConfigValue("second.prod.price", String.valueOf(Float.parseFloat(price.trim().replace("Rs.",""))));
+        ConfigReader.setConfigValue("second.prod.price", String.valueOf(Float.parseFloat(price.trim().replace("Rs.", ""))));
 //        WebElement chooseOpt=driver.findElement(By.xpath("(//quick-view-button)[2]"));
-        actions.moveToElement(secondProdName).click().build().perform();
+        actions.moveToElement(secondProdName).build().perform();
+        secondProd.click();
     }
 }
